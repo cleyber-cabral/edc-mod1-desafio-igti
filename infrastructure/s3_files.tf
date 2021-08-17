0a67678f37a0a8b28cd9a30f6c04c621caa7e67e
cleyber-cabral <cleyber.cabral@gmail.com>
2021-08-17 16:57:47 -0300

    ajuste

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -2,6 +2,8 @@ resource "aws_s3_bucket_object" "raw-data" {
   bucket = aws_s3_bucket.dl.id
   key    = "raw-data/censo/year=2020"
   acl    = "private"
-  source = "data/microdados_educacao_basica_2020/DADOS/matricula_co.csv"
+  source = "data/DADOS/matricula_co.csv"
 #   etag   = filemd5("matricula_co.csv")
 }
+
+# nova pasta
