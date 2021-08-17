f4fc75a0fa7e5b44d777d3112ab0a8e94c9248a2
cleyber-cabral <30348791+cleyber-cabral@users.noreply.github.com>
2021-08-17 16:24:40 -0300

    Update s3_files.tf

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket_object" "raw-data" {
   bucket = aws_s3_bucket.dl.id
   key    = "raw-data/censo/year=2020"
   acl    = "private"
-  source = "../data/microdados_educacao_basica_2020/dados/matricula_co.csv"
+  source = "./data/microdados_educacao_basica_2020/dados/matricula_co.csv"
 #   etag   = filemd5("matricula_co.csv")
 }
