8785b0f445d413b42fc949387c5459764cb103d4
cleyber-cabral <cleyber.cabral@gmail.com>
2021-08-17 16:19:31 -0300

    corrigindo o s3_files

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -3,5 +3,5 @@ resource "aws_s3_bucket_object" "raw-data" {
   key    = "raw-data/censo/year=2020"
   acl    = "private"
   source = "../data/microdados_educacao_basica_2020/dados/matricula_co.csv"
-  etag   = filemd5("matricula_co.csv")
+#   etag   = filemd5("matricula_co.csv")
 }
