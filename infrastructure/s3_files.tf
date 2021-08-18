cf2f6bf08bc433dfb893cdd9c34c7a7f7023bdab
cleyber-cabral <cleyber.cabral@gmail.com>
2021-08-18 10:37:32 -0300

    Tentativa 1

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -1,8 +1,16 @@
-resource "aws_s3_bucket_object" "raw-data" {
+# Faz o upload do arquivo para o s3
+resource "aws_s3_bucket_object" "matricula_co" {
   bucket = aws_s3_bucket.dl.id
   key    = "raw-data/censo/year=2020"
   acl    = "private"
   source = "../data/DADOS/matricula_co.csv"
-#   etag   = filemd5("matricula_co.csv")
+  etag   = filemd5("matricula_co.csv")
 }
 
+# resource "aws_s3_bucket_object" "matricula_co" {
+#   bucket = aws_s3_bucket.dl.id
+#   key    = "raw-data/censo/year=2020"
+#   acl    = "private"
+#   source = "../data/DADOS/matricula_co.csv"
+#   etag   = filemd5("matricula_co.csv")
+# }
