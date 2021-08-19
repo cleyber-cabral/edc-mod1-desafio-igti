e4764097f31743fc3697d356b67cb1f38ddc75fb
cleyber-cabral <cleyber.cabral@gmail.com>
2021-08-18 21:11:26 -0300

    Alteração

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -1,11 +1,4 @@
 # Faz o upload do arquivo para o s3
-resource "aws_s3_bucket_object" "interact_s3" {
-  bucket = aws_s3_bucket.dl.id
-  key    = "emr-code/pyspark/interact_s3.py"
-  acl    = "private"
-  source = "../etl/interact_s3.py"
-  etag   = filemd5("../etl/interact_s3.py")
-}
 resource "aws_s3_bucket_object" "JobSpark" {
   bucket = aws_s3_bucket.dl.id
   key    = "emr-code/pyspark/JobSpark.py"
